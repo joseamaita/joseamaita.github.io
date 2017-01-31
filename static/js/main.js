8059f0371f0b5daf7655924a4a77edbb9c07e148
José Alberto Maita Fariñas <jose_a_maita@hotmail.com>
2017-01-31 17:27:19 -0500

    Update style for pre element in main.css and main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,5 +2,4 @@ $(document).ready(function() {
 	// console.log('Message here !!!');
 	// alert('Message here !!!');
 	$('table').addClass("table table-bordered table-hover");
-	$('pre').css("fontSize", "12px");
 });
