61183abfe44bd97734160bc9153455fbd8352f3c
José Alberto Maita Fariñas <jose_a_maita@hotmail.com>
2017-01-30 22:02:47 -0500

    Edit content in python folder

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
 	// console.log('Message here !!!');
 	// alert('Message here !!!');
 	$('table').addClass("table table-bordered table-hover");
+	$('pre').css("fontSize", "12px");
 });
