2feab2b3cdae33b86011fdb39e3a8b8fa2603b1f
José Alberto Maita Fariñas <jose_a_maita@hotmail.com>
2017-02-10 10:17:00 -0500

    Test the process of showing and hiding content

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
 	// console.log('Message here !!!');
 	// alert('Message here !!!');
 	$('table').addClass("table table-bordered table-hover");
+	
+	var $more-content = $('.more-content').first();
+	$('a.more').click(function(event) {
+		event.preventDefault();
+		$more-content.slideToggle('slow');
+		var $link = $(this);
+		if ($link.text() == 'read more') {
+			$link.text('read less');
+		}
+		else {
+			$link.text('read more');
+		}
+	});
 });
