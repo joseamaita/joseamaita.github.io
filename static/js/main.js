603d3397cf34953eb461c6aeeccc3b7ac8319a6e
José Alberto Maita Fariñas <jose_a_maita@hotmail.com>
2017-02-10 10:24:10 -0500

    Test the process of showing and hiding content

--- static/js/main.js
+++ static/js/main.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
 	var $more-content = $('.more-content').first();
 	$('a.more').click(function(event) {
 		event.preventDefault();
+		console.log('Clicked here');
+		console.log($more-content);
 		$more-content.slideToggle('slow');
 		var $link = $(this);
 		if ($link.text() == 'read more') {
