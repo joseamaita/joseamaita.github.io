a059108c2b17c9a1500ced4cb10057c1cea3f06e
José Alberto Maita Fariñas <jose_a_maita@hotmail.com>
2017-02-16 16:34:34 -0500

    Change read more and read less symbols

--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 	$('a.read-more-less').click(function(event) {
 		event.preventDefault();
 		var $moreContent = $(this).parent().prev();
-		console.log($moreContent);
 		$moreContent.slideToggle('slow');
 		var $link = $(this);
 		if ($link.text() == 'read more »') {
